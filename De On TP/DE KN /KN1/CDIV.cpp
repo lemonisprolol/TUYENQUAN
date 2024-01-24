bcd807ee2e0c604778f7a8c871f96b24f03bb6e7
Lemon <93773512+lemonisprolol@users.noreply.github.com>
2024-01-24 10:36:02 +0700

    Update CDIV.cpp

--- De On TP/DE KN /KN1/CDIV.cpp
+++ De On TP/DE KN /KN1/CDIV.cpp
@@ -2,30 +2,46 @@
 
 using namespace std;
 
-const int maxarr = 2 * 1e5;
-long a[maxarr];
-int main()
+void inoutput()
 {
     iostream::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     freopen("CDIV.INP", "r", stdin);
     freopen("CDIV.OUT", "w", stdout);
+}
+const int maxarr = 2 * 1e5;
+const int maxnum = 1e6 + 5;
+long a[maxarr];
+long b[maxnum];
+int main()
+{
+    inoutput();
     int n;
     cin >> n;
     for(int i = 1; i <= n; i++)
     {
         cin >> a[i];
+        for(long long j = 1; j * j <= a[i]; j++)
+        {
+            if(a[i] % j == 0)
+            {
+                b[j]++;
+                if(a[i ] % (a[i] / j) == 0 &&(a[i] / j) != j)
+                {
+                    b[a[i] / j]++;
+                }
+            }
+        }
     }
-    long gcd = LONG_MIN;
-    long MAX = a[1];
-    for(int i = 2; i <= n; i++)
+    for(int i = maxnum; i >= 2; i--)
     {
-        gcd = max(gcd, abs(__gcd(MAX, a[i])));
-
-        MAX = max(MAX,a[i]);
+        if(b[i] >= 2)
+        {
+            cout<<i;
+            return 0;
+        }
     }
-    cout<<gcd;
 
     return 0;
 }
