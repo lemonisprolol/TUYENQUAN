ed022ec48eb07c3d3c62b9b0aeecf394844967e1
Lemon <93773512+lemonisprolol@users.noreply.github.com>
2024-01-26 10:33:23 +0700

    Update WORK.cpp

--- De On TP/DE KN /KN 2/WORK.cpp
+++ De On TP/DE KN /KN 2/WORK.cpp
@@ -1,40 +1,45 @@
-#include <bits/stdc++.h>
+#include <bits./stdc++.h>
 
 using namespace std;
-
-struct timeatt
-{
-    int one;
-    int two;
-};
-const int maxarr = 1e6 + 5;
-timeatt a[maxarr];
-long s[maxarr];
-int main()
+void inoutput()
 {
     iostream::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     freopen("WORK.INP", "r", stdin);
     freopen("WORK.OUT", "w", stdout);
+}
+const int maxarr = 1e6 + 5;
+
+struct Time
+{
+    int one;
+    int two;
+};
+Time a[maxarr];
+int s[maxarr]; //best solution until s[i]
+
+int main()
+{
+    inoutput();
     int n;
     cin >> n;
     for(int i = 1; i <= n; i++)
     {
         cin >> a[i].one;
-
     }
     for(int i = 1; i < n; i++)
     {
         cin >> a[i].two;
     }
-    long work = 0;
-    for(int i = 1; i <= n; i++)
+    a[0].one = 0, a[0].two = 0;
+    s[1] = a[1].one;
+    for(int i = 2; i <= n; i++)
     {
-        //cout<<i<<" "<<a[i].one<<" "<<a[i].two<<endl;
-        s[i] = min(s[])
+        s[i] = min(s[i - 1] + a[i].one, s[i - 2] + a[i - 1].two);
 
     }
-    cout<<work;
+    cout<<s[n];
+
     return 0;
 }
