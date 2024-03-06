788d61b2edf3ddb22bbcdea8c44eaac96a36c584
Lemon <93773512+lemonisprolol@users.noreply.github.com>
2024-03-06 19:47:09 +0700

    Update TBC.cpp

--- De On TP/TPHCM/2022-2023/TBC.cpp
+++ De On TP/TPHCM/2022-2023/TBC.cpp
@@ -1,34 +1,33 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 void inoutput()
 {
     iostream::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("TCB.INP", "r", stdin);
-    freopen("TCB.OUT", "w", stdout);
+    freopen("TBC.INP", "r", stdin);
+    freopen("TBC.OUT", "w", stdout);
 }
 const int maxarr = 1e6 + 5;
-long a[maxarr];
-long b[maxarr];
+long long a[maxarr];
+long long b[maxarr];
 int main()
 {
     inoutput();
     int n;
-    cin >> n;
+    cin >>n;
     for(int i = 1; i <= n; i++)
     {
-        cin >> b[i];
+        cin >> a[i];
     }
-    long long sum = 0;
+    long long sumcur = 0;
     for(int i = 1; i <= n; i++)
     {
-        long long temp = b[i] * i;
-        a[i] = temp - sum;
-        sum += b[i];
-        cout<<a[i]<<" ";
-
+        b[i] = a[i] * i - sumcur;
+        sumcur += b[i];
+        cout<<b[i]<<" ";
     }
 
 
