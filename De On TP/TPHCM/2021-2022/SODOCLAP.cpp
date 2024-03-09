5cca3037bdac3f0b4f437f4d2774146114099349
Lemon <93773512+lemonisprolol@users.noreply.github.com>
2024-03-09 11:13:30 +0700

    Update SODOCLAP.cpp

--- De On TP/TPHCM/2021-2022/SODOCLAP.cpp
+++ De On TP/TPHCM/2021-2022/SODOCLAP.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void inouput()
+
+void inoutput()
 {
     iostream::sync_with_stdio(0);
     cin.tie(0);
@@ -9,57 +10,87 @@ void inouput()
     freopen("SODOCLAP.INP", "r", stdin);
     freopen("SODOCLAP.OUT", "w", stdout);
 }
+map<char, int> d;
 
-int a[100];
-char findandfill(string n, int position, bool cases, char x)
+string taoso(int anchor, string s)
 {
-
-
-    for(int i = (int) '0'; i <= (int)'9'; i++)
+    bool found = false;
+    while(!found)
     {
-        if(a[i] == 0)
+        for(char j = '0'; j <= '9'; j++)
         {
-            if(cases == true)
+            //cout<<s<<endl;
+
+
+            if(s[anchor] < j && d[j] == 0)
             {
-                if(x > (char) i)
-                {
-                    continue;
-                }
+                s[anchor] = j;
+                d[j]++;
+                found = true;
+                break;
             }
-            return (char)i;
+        }
+        if(!found)
+        {
+            s[anchor] = '0';
+            anchor--;
         }
     }
+
+
+
+    for(int i = anchor + 1 ; i < s.size(); i++)
+    {
+        for(int j = '0'; j <= '9'; j++)
+        {
+            //cout<<i<<" "<<d[j]<<" "<<j<<endl;
+            if(d[j] <= 0 )
+            {
+               // cout<<d[j]<<endl;
+               d[s[j]]--;
+                s[i] = j;
+
+
+                d[j]++;
+                break;
+
+            }
+        }
+
+
+
+    }
+    return s;
 
 }
 int main()
 {
-    inouput();
-    string n;
-    cin >> n;
-    int k_pos;
-    bool cases = true;
-    for(int i = 0; i < n.size(); i++)
+    inoutput();
+    string s;
+    cin >> s;
+    s = '0' + s;
+    int anchor = s.size() - 1;
+
+    for(int i = 1; i < s.size(); i++)
     {
-        a[n[i]]++;
-        //cout<<a[n[i]]<<endl;
-        if(a[n[i]] == 2)
+        d[s[i]]++;
+        if(d[s[i]] > 1)
         {
-            k_pos = i;
-            a[n[i]]--;
-            n[i] = findandfill(n, k_pos, cases, n[i]);
-            a[n[i]]++;
+            anchor = i;
+            d[s[i]]--;
             break;
 
         }
     }
-    cases = false;
-    for(int i = k_pos + 1; i < n.size(); i++)
-    {
-            n[i] = findandfill(n, k_pos, cases, n[i]);
-            a[n[i]]++;
 
+
+    string ans = taoso(anchor, s);
+    if(ans[0] == '0')
+    {
+        ans.erase(ans.begin() + 0);
     }
-    cout<<n;
+    cout<<ans;
+
 
     return 0;
 }
