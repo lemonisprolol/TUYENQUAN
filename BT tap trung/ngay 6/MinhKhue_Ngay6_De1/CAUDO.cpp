382a3e158dd3c226f11dc45de0653a17afdb2bbf
Lemon <93773512+lemonisprolol@users.noreply.github.com>
2024-03-11 10:31:34 +0700

    Create CAUDO.cpp

---
+++ BT tap trung/ngay 6/MinhKhue_Ngay6_De1/CAUDO.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("CAUDO.INP", "r", stdin);
+    freopen("CAUDO.OUT", "w", stdout);
+
+    long n;
+    cin >> n;
+    string ans = "";
+    while(n > 0)
+    {
+        char temp = (char) (n % 2 + '0');
+        ans = temp + ans;
+        n /= 2;
+    }
+    //cout<<ans;
+    while(ans.size() < 5)
+    {
+        ans = '0' + ans;
+    }
+    for(int i = 0; i < ans.size() - 1; i++)
+    {
+        if(ans[i] == '1') cout<<"A"<<" ";
+        else cout<<"O ";
+    }
+    if(ans[ans.size() - 1] == '1') cout<<"A";
+    else cout<<"O";
+    return 0;
+}
