99e7c3d3acdaa43f206bcb7571b7143cc80512be
Lemon <93773512+lemonisprolol@users.noreply.github.com>
2024-03-17 21:21:23 +0700

    Update MATMA.cpp

--- De On TP/TPHCM/2022-2023/MATMA.cpp
+++ De On TP/TPHCM/2022-2023/MATMA.cpp
@@ -1,30 +1,50 @@
+// AC solution
+
 #include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(0);
+
+void inoutput()
+{
+    iostream::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("MATMA.inp","r",stdin);
-    freopen("MATMA.out","w",stdout);
-    long long n , m , k , x , counter = 0;
+    freopen("MATMA.INP", "r", stdin);
+    freopen("MATMA.OUT", "w", stdout);
+}
+string ma[505];
+int main()
+{
+    inoutput();
+    int m, n, k , x;
+    cin >> n >> m>> k >> x;
     string s;
-    cin>>n>>m>>k>>x>>s;
-    string a[m];
-    char c[m];
+    cin >> s;
     x--;
-    for(int i = 0 ; i < m ; i++)
-        cin>>a[i];
-    for(int i = m - 1 ; i >= 0 ; i--){
-        c[i] = a[i][x % k];
+    for(int i = 1; i <= m; i++)
+    {
+        cin >> ma[i];
+        sort(ma[i].begin(), ma[i].end());
+    }
+    vector<char> org;
+    for(int i = m; i >= 1; i--)
+    {
+        org.push_back(ma[i][x % k]);
         x /= k;
     }
-    for(int i = 0; i < s.size() ; i++){
-        if(s[i] != '#')
-            cout<<s[i];
-        else{
-            cout<<c[counter];
-            counter++;
+    int y = org.size() - 1;
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == '#')
+        {
+            s[i] = org[y];
+            y--;
         }
     }
+    cout<<s;
+
+
+
+
     return 0;
 }
